066db5ff4c60d132d52e5b247ce0965b7cf43c05
anamariagr <anamariagarcia9709@gmail.com>
2019-09-15 23:14:10 -0500

    sectionfive

--- src/componentes/SectionFive.js
+++ src/componentes/SectionFive.js
@@ -5,10 +5,10 @@ const SectionFive = props =>{
         return(
             <div className="container">
                 <div className="row ">
-                    <div className="col-lg-6 mt-lg-5 pt-lg-5">
+                    <div className="col-lg-6 mt-lg-4 pt-lg-5">
                          <Cards/>
                     </div>
-                   <div className="col-md-6  mt-lg-5 pt-lg-5 pl-lg-5">
+                   <div className="col-md-6  mt-lg-4 pt-lg-5 pl-lg-5">
                     <div className="row">
                         <div className="col-md-12">
                             <div className="row letter-color pt-0">
@@ -24,19 +24,18 @@ const SectionFive = props =>{
                                         </div>
                                         </div>
                                         
-                                      
                                         <div className="col-md-12">
                                         <p>
                                             Los emprendedores necesitan herramientas que se ajusten a sus ritmos acelerados de trabajo y más si cuando están pasando por un proceso de incubación o aceleración, nuestra plataforma registra toda la información del proyecto emprendedor y se va actualizando constantemente durante la evolución que se va teniendo en el tiempo.
                                             </p>
                                         </div>
-                                        <div className="col-md-12">
+                                        <div className="col-md-12 mt-4">
                                                 <img here=" # " alt=" " className="img-zise" src="./img/section_6.png"/> 
                                         </div>
                                        
                                         <div className="col-md-12 pt-5 text-center ">
-                                        <a target="_blank" href="https://meetwork.space"><button type="button"  className="btn btn-download shadow   bg-white">Activar mi establecimiento</button> </a>
-                                            </div>
+                                        <a rel="noopener noreferrer" target="_blank" href="https://meetwork.space"><button type="button"  className="btn btn-download shadow   bg-white">Activar mi establecimiento</button> </a>
+                                            </div> 
                                     
                                     
                                   
