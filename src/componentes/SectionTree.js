da9fae0815be962cf598334ef2ac403eb90e2b11
anamariagr <anamariagarcia9709@gmail.com>
2019-09-15 23:16:51 -0500

    section three

--- src/componentes/SectionTree.js
+++ src/componentes/SectionTree.js
@@ -5,13 +5,13 @@ const SectionTree = props =>{
     return(
 
         <div className="container">
-             <div className="row letter-color">
-                   <div className="col-md-5">
+             <div className="row letter-color-Three">
+                   <div className="col-md-5 pr-5">
                        <div className="row">
                             
                                         <div className="col-md-12 pb-5">
                                             <p className="subtitle-three">
-                                            ¿ Colaboras con emprendedores ?
+                                            ¿Colaboras con emprendedores?
                                                 </p>
                                             <div className="line-three">
                                                 
@@ -31,25 +31,18 @@ Hemos desarrollado una herramienta para gestionar comunidades de Startups a trav
                                         Los emprendedores necesitan herramientas que se ajusten a sus ritmos acelerados de trabajo y más si cuando están pasando por un proceso de incubación o aceleración, nuestra plataforma registra toda la información del proyecto emprendedor y se va actualizando constantemente durante la evolución que se va teniendo en el tiempo.
                                             </p>
                                         </div>
-                                        <div className="col-md-12">
+                                        
+                                        <div className="col-md-12 ">
                                                 <p className="title-sub"> 
                                                 Que es lo que realmente esta sucediendo?
                                                 </p>
                                             </div>
-                                            <div className="col-md-12">
+                                            <div className="col-md-12 mb-4 pb-2">
                                                 <p>
                                                 Todos los días los emprendedores encuentran nuevos desafíos por resolver, y necesitan tomar decisiones rápidas, estas se deberían fundamentar en la información clave que mueve su negocio, nuestra plataforma ayuda a las incubadoras y aceleradoras a comunicarse con los emprendedores mostrándoles información clave de los avances y así poder tomar decisiones estratégicas.
                                                 </p>
                                             </div>
-                                        <div className="col-md-12">
-                                        <a target="_blank" href="https://meetwork.cl/login"><button type="button" class="btn btn-download shadow   bg-white">Iniciar una prueba gratis</button></a>
-                                        </div>
-                                        <div className="col-md-12 p-3">
-                                                <p >
-                                                <b>Que es lo que realmente esta sucediendo?</b>
-                                                </p>
-                                            </div>
-                                            <div className="col-md-12">
+                                            <div className="col-md-12 mb-4 pb-2">
                                                 <div className="row">
                                                     <div className="col col-sm-3">
                                                         <img alt="logo-4" className="size-logo" src="./img/startup.png"/>
@@ -58,13 +51,22 @@ Hemos desarrollado una herramienta para gestionar comunidades de Startups a trav
                                                         <img alt="logo-5" className="size-logo" src="./img/simon.png"/>
                                                     </div>
                                                     <div className="col col-sm-3">
-                                                        <img  alt="logo-6" src="./img/logo_digevo.png"/>
+                                                        <img  alt="logo-6" className="size-logo" src="./img/logo_digevo.png"/>
                                                     </div>
                                                     <div className="col col-sm-3">
-                                                        <img   alt="logo-7" src="./img/logo_social_lab.png"/> 
+                                                        <img   alt="logo-7" className="size-logo" src="./img/logo_social_lab.png"/> 
                                                     </div>
                                                 </div>
                                             </div>
+                                            <div className="col-md-12 p-3">
+                                                <p >
+                                                Nos basamos en procesos de aceleración de aceleradoras de startups como:                                                </p>
+                                            </div>
+                                        <div className="col-md-12">
+                                        <a rel="noopener noreferrer" target="_blank" href="https://meetwork.cl/login"><button type="button" class="btn btn-download shadow   bg-white">Iniciar una prueba gratis</button></a>
+                                        </div>
+                                       
+                                           
                                             <div className="col-md-12 ground-float-one"></div>
                                        
                        </div>
